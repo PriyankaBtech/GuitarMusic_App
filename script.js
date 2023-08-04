b43cedee1186d0390a67fae423f97ac28328f13f
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-08-04 19:33:37 +0530

    update song list

--- script.js
+++ script.js
@@ -1,37 +1,44 @@
 // Declare variables
-let songElement = new Audio('./Photos/music.mp3')
+let songElement = new Audio('./Music/music.mp3')
 let songIndex = 0
 let centerPlay = document.getElementById('centerPlay')
 let progressBar = document.getElementById('progressBar')
 let gif = document.getElementById('gif')
+let songItems = Array.from(document.getElementsByClassName('songItems'))
 
 
 let songs = [
-    {songName: "summar-walk",
-     filePath: "./Photos/music.mp3",
-     coverPath: "./Photos/cover.jpg"
+    {songName: "Summar-walk",
+     filePath: "./Music/music.mp3",
+     coverPath: "./Photos/cover.jpg"   
     },
-    {songName: "summar-walk",
-     filePath: "./Photos/music.mp3",
-     coverPath: "./Photos/cover.jpg"
+    {songName: "Forest-lullbaby",
+     filePath: "./Music/music1.mp3",
+     coverPath: "./Photos/cover.jpg"   
     },
-    {songName: "summar-walk",
-     filePath: "./Photos/music.mp3",
-     coverPath: "./Photos/cover.jpg"
+    {songName: "Reflected-light",
+     filePath: "./Music/music2.mp3",
+     coverPath: "./Photos/cover.jpg"   
     },
-    {songName: "summar-walk",
-     filePath: "./Photos/music.mp3",
-     coverPath: "./Photos/cover.jpg"
+    {songName: "The beat of nature",
+     filePath: "./Music/music3.mp3",
+     coverPath: "./Photos/cover.jpg"   
     },
-    {songName: "summar-walk",
-     filePath: "./Photos/music.mp3",
-     coverPath: "./Photos/cover.jpg"
+    {songName: "Waterfall",
+     filePath: "./Music/music4.mp3",
+     coverPath: "./Photos/cover.jpg"   
     },
-    {songName: "summar-walk",
-     filePath: "./Photos/music.mp3",
-     coverPath: "./Photos/cover.jpg"
-    }
+    {songName: "Electronic-rock",
+     filePath: "./Music/music5.mp3",
+     coverPath: "./Photos/cover.jpg"   
+    },
 ]
+
+// looping
+songItems.forEach((element, i) => {
+    console.log(element, i)
+    element.getElementsByClassName("songNames")[0].innerText = songs[i].songName
+})
 
 
 //handle play and pause click
